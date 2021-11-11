baf6d5934f8493de716e7cad4bb2f1876140dd0b
James Dai <james.dai@metadataconsulting.co.uk>
2021-11-11 18:59:14 +0000

    Script to convert Production files to Pre-Production files

---
+++ convertProdToPreprod.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+URL1="https://ipcc.metadata.works";
+URL2="https://ipcc-preprod-browser.metadatadev.co.uk";
+echo "Replacing $URL1 with $URL2"...
+sed -i '' "s,${URL1},${URL2},g" "data_catalogue.html";
+sed -i '' "s,${URL1},${URL2},g" "_site/data_catalogue.html";
+echo "Success!"
+
+PROD="G-2WXQ7WQ7JW";
+PREPROD="G-0M26637VMX";
+PROD_COMMENT='"ipcc-\[production-browser\]" GA Property';
+PREPROD_COMMENT='"ipcc-preprod-browser" GA Property';
+
+echo "Replacing PROD: $PROD with PREPROD: $PREPROD"...
+
+## declare an array variable
+declare -a filenames=(
+"about.html" "copyright.html"
+"data_catalogue.html" "disclaimer.html"
+"embeded-tools.html" "index.html"
+"privacy-policy.html"
+"_layouts/alt.html" "_layouts/ddc02.html"
+"_layouts/ddc_simple_banner.html" "_layouts/default.html")
+
+## now loop through the above array
+for filename in "${filenames[@]}"
+do
+   echo "Processing $filename..."
+   sed -i '' "s,${PROD},${PREPROD},g" "$filename";
+   sed -i '' "s,${PROD},${PREPROD},g" "_site/$filename";
+
+   sed -i '' "s,${PROD_COMMENT},${PREPROD_COMMENT},g" "$filename";
+   sed -i '' "s,${PROD_COMMENT},${PREPROD_COMMENT},g" "_site/$filename";
+done
+
+echo "Success!"
