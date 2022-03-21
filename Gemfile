3375c50c532e519ef13d2ce56395c56ee061d403
James Dai <james.dai@metadataconsulting.co.uk>
2022-03-21 12:37:25 +0000

    Revert "Merge branch 'EX-6935-cookie-consent-updates' into AR6-update"
    This reverts commit 257771266ce52811e9678c0da06ddb93be143898.

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
-source "https://rubygems.org"
 
-gem "jekyll", "~> 4.2.0"
+
 ## add support for constants defined in _data/constants.yml
 ## see https://github.com/unabris/jekyll_constant_values
-gem 'jekyll_constant_values', group: :jekyll_plugins
-gem "jekyll-theme-modernist", "~> 0.2"
 
+gem 'jekyll_constant_values', group: :jekyll_plugins
+gem "jekyll", "~> 4.2.0"
+gem "jekyll-theme-modernist", "~> 0.2"
 gem "webrick", "~> 1.7"
--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,8 +1,18 @@
-function initialiseGoogleAnalytics() {
-    console.log("Initialising Google Analytics...")
+var head = document.getElementsByTagName('head')[0];
+var googleAnalyticsScript = document.createElement('script');
+var GOOGLE_ANALYTICS_TRACKING_ID = 'G-2WXQ7WQ7JW';
+googleAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + GOOGLE_ANALYTICS_TRACKING_ID;
+head.appendChild(googleAnalyticsScript);
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+window.gtag = gtag;
+
+// If consent cookie exists then initialise tracking
+if (document.cookie === 'google_analytics_user_consented=yes') {
     var head = document.getElementsByTagName('head')[0];
     var googleAnalyticsScript = document.createElement('script');
-    var GOOGLE_ANALYTICS_TRACKING_ID = 'G-9X9X51FDVZ'; // Measurement ID for Web stream with URL = "https://ipcc-data.org"
     googleAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + GOOGLE_ANALYTICS_TRACKING_ID;
     head.appendChild(googleAnalyticsScript);
     window.dataLayer = window.dataLayer || [];
@@ -10,13 +20,8 @@ function initialiseGoogleAnalytics() {
         dataLayer.push(arguments);
     }
     window.gtag = gtag;
-}
-
-// If consent cookie exists then initialise tracking
-if (document.cookie.includes('google_analytics_user_consented=yes')) {
-    initialiseGoogleAnalytics()
 } else if (!document.cookie.includes('google_analytics_user_consented')) {
-    $('#myModal').modal('show');
+    $('#myModal').modal();
 }
 
 function setUpCookieExpiration(days) {
@@ -28,7 +33,6 @@ function setUpCookieExpiration(days) {
 function acceptCookie() {
     var expires = setUpCookieExpiration(28);
     document.cookie = 'google_analytics_user_consented=yes' + expires + '; path=/';
-    initialiseGoogleAnalytics()
 }
 
 function declineCookie() {
