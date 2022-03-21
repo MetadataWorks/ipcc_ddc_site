257771266ce52811e9678c0da06ddb93be143898
James Dai <james.dai@metadataconsulting.co.uk>
2022-03-21 12:37:25 +0000

    Merge branch 'EX-6935-cookie-consent-updates' into AR6-update

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,18 +1,8 @@
-var head = document.getElementsByTagName('head')[0];
-var googleAnalyticsScript = document.createElement('script');
-var GOOGLE_ANALYTICS_TRACKING_ID = 'G-2WXQ7WQ7JW';
-googleAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + GOOGLE_ANALYTICS_TRACKING_ID;
-head.appendChild(googleAnalyticsScript);
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-window.gtag = gtag;
-
-// If consent cookie exists then initialise tracking
-if (document.cookie === 'google_analytics_user_consented=yes') {
+function initialiseGoogleAnalytics() {
+    console.log("Initialising Google Analytics...")
     var head = document.getElementsByTagName('head')[0];
     var googleAnalyticsScript = document.createElement('script');
+    var GOOGLE_ANALYTICS_TRACKING_ID = 'G-9X9X51FDVZ'; // Measurement ID for Web stream with URL = "https://ipcc-data.org"
     googleAnalyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + GOOGLE_ANALYTICS_TRACKING_ID;
     head.appendChild(googleAnalyticsScript);
     window.dataLayer = window.dataLayer || [];
@@ -20,8 +10,13 @@ if (document.cookie === 'google_analytics_user_consented=yes') {
         dataLayer.push(arguments);
     }
     window.gtag = gtag;
+}
+
+// If consent cookie exists then initialise tracking
+if (document.cookie.includes('google_analytics_user_consented=yes')) {
+    initialiseGoogleAnalytics()
 } else if (!document.cookie.includes('google_analytics_user_consented')) {
-    $('#myModal').modal();
+    $('#myModal').modal('show');
 }
 
 function setUpCookieExpiration(days) {
@@ -33,6 +28,7 @@ function setUpCookieExpiration(days) {
 function acceptCookie() {
     var expires = setUpCookieExpiration(28);
     document.cookie = 'google_analytics_user_consented=yes' + expires + '; path=/';
+    initialiseGoogleAnalytics()
 }
 
 function declineCookie() {
--- convertProdToPreprod.sh
+++ convertProdToPreprod.sh
@@ -9,21 +9,16 @@ sed -i '' "s,${URL1},${URL2},g" "ar6landing.html";
 sed -i '' "s,${URL1},${URL2},g" "_site/ar6landing.html";
 echo "Success!"
 
-PROD="G-2WXQ7WQ7JW";
+PROD="G-9X9X51FDVZ";
 PREPROD="G-0M26637VMX";
-PROD_COMMENT='"ipcc-\[production-browser\]" GA Property';
-PREPROD_COMMENT='"ipcc-preprod-browser" GA Property';
+PROD_COMMENT='Measurement ID for Web stream with URL = "https://ipcc-data.org"';
+PREPROD_COMMENT='Measurement ID for Web stream with URL = "https://ipcc-preprod.metadatadev.co.uk"';
 
 echo "Replacing PROD: $PROD with PREPROD: $PREPROD"...
 
 ## declare an array variable
 declare -a filenames=(
-"about.html" "copyright.html"
-"data_catalogue.html" "disclaimer.html"
-"embeded-tools.html" "index.html"
-"privacy-policy.html"
-"_layouts/alt.html" "_layouts/ddc02.html"
-"_layouts/ddc_simple_banner.html" "_layouts/default.html")
+"assets/js/analytics.js")
 
 ## now loop through the above array
 for filename in "${filenames[@]}"
